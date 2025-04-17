6dfb5596bf44017752283182081907205c6949e4
Vinicius Santos <viniciussantos1314@gmail.com>
2025-04-17 11:24:11 -0300

    prod env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,12 +8,6 @@ import router from './router'
 
 const app = createApp(App)
 
-if (import.meta.env.PROD) {
-    app.config.devtools = false
-    app.config.debug = false
-    app.config.warnHandler = () => { } // silencia os avisos também
-}
-
 app.use(createPinia())
 app.use(router)
 
